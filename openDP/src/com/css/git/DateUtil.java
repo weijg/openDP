c999f893d5f1f5b4fa8b0d0dd4606fe019040e64
weijg <weijiguang1@163.com>
2013-04-11 14:06:08 +0800

    增加 tools() 方法！

--- openDP/src/com/css/git/DateUtil.java
+++ openDP/src/com/css/git/DateUtil.java
@@ -14,7 +14,11 @@ public class DateUtil {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("************");
+		System.out.println("****123********");
+	}
+	
+	public void tools(){
+		
 	}
 
 }
