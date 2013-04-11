6a0a1bb72f1ab6770e220a12e5b73cbde7c6a9bb
weijg <weijiguang1@163.com>
2013-04-11 13:58:00 +0800

    FistGit add  helloGit()
    Change-Id: I8eac110bea517b03fe6c96a3dbfb27596937ab2a
    Signed-off-by: weijg <weijiguang1@163.com>

---
+++ openDP/src/com/css/git/DateUtil.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.css.git;
+
+/**
+ * @author weijg
+ *
+ */
+public class DateUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
